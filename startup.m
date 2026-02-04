381eb68ac6f61f3c146313fae68d4a5413b10dae
owenwrey <owenwrey@gmail.com>
2026-02-04 13:54:51 -0600

    increased startup.m figure lineWidth
    increased startup.m figure lineWidth

--- startup.m
+++ startup.m
@@ -4,4 +4,8 @@
 % Make figure backgrounds white
 set(groot, 'defaultFigureColor', 'white');
 
+% Make figure LineWidth 1.75 by default
+set(groot, 'DefaultLineLineWidth', 1.75)
+
+
 disp("startup.m ran");
