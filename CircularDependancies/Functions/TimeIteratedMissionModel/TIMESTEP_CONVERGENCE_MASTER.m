76a8f2861dc3fe47ac9fd4b0a54b3a83bbf99d40
owenwrey <owenwrey@gmail.com>
2026-02-23 13:54:12 -0600

    added TimeStep funcs to CircularDep function folder

---
+++ CircularDependancies/Functions/TimeIteratedMissionModel/TIMESTEP_CONVERGENCE_MASTER.m
@@ -0,0 +1,37 @@
+function aircraft = TIMESTEP_CONVERGENCE_MASTER(aircraft, missionToRun)
+% inputs: aircraft (structure)
+%         missionToRun: a string that is either 'A2A' or 'Strike' or 'Both'
+
+Results = struct;
+
+missionToRun = string(missionToRun);
+
+switch missionToRun
+
+    case "A2A"
+        Results.A2A = A2A(aircraft);
+        aircraft = Results.A2A;
+
+    case "Strike"
+        Results.Strike = Strike(aircraft);
+        aircraft = Results.Strike;
+
+    case "Both"
+        Results.A2A = A2A(aircraft);       
+        Results.Strike = Strike(aircraft);
+
+        if Results.A2A.weight.total > Results.Strike.weight.total
+            aircraft.constrainingMission = "A2A";
+            aircraft = Results.A2A;
+        elseif Results.Strike.weight.total > Results.A2A.weight.total
+            aircraft.constrainingMission = "Strike";
+            aircraft = Results.Strike;
+        end
+
+    otherwise
+        error("Unknown mission type: %s", missionToRun)
+
+end % switch
+
+
+end % function
