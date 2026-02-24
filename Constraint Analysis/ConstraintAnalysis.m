1e390b9822f7f153c68287a26cb15d36a0b73099
laurnk0823-sudo <laurnk0823@gmail.com>
2026-02-24 12:20:04 -0600

    Update ConstraintAnalysis.m

--- Constraint Analysis/ConstraintAnalysis.m
+++ Constraint Analysis/ConstraintAnalysis.m
@@ -18,7 +18,7 @@ TOP = 300;
 
 %% ISA + Delta T Setup
 
-DeltaT = 17;           % ISA deviation [K]
+DeltaT = 32-15;           % ISA deviation [K]
 gamma = 1.4;
 R = 287.05;
 
@@ -224,7 +224,7 @@ xline(W2S_Stall.*0.02088547,'Color',purple,'LineWidth',1.5)
 colormap(jet)
    
 
-plot(110,0.85,'o')
+plot(112,0.82,'o')
 
 xlabel("Wing Loading (lb/ft^2)")
 ylabel("Thrust-to-Weight")
