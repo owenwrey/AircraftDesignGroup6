2ae701a55f356a630a43dedba0a9911b70c80615
acarpet <flyingcarpetjunior@gmail.com>
2026-03-02 14:42:12 -0600

    EWB in main

--- CircularDependancies/Main.m
+++ CircularDependancies/Main.m
@@ -38,6 +38,15 @@ aircraft.weight.totalOnLanding = 42e3;
 
 % General
 aircraft.constants.wingLoading = 112; % [lbf/ft]
+aircraft.engine.weight = 3920;
+aircraft.engine.thrust = 29000;
+aircraft.engine.TSFC = .67;
+
+aircraft.constants.fuelVolume = 3500;
+aircraft.fuelSys.VI = 0;                % integral fuel volume, gal
+aircraft.fuelSys.VP = aircraft.constants.fuelVolume/2;  % self-sealing tanks volume, gal
+aircraft.fuelSys.Nt = 4;
+
 
 % Geometry
 aircraft.fuselage.length   = 48;
@@ -45,23 +54,36 @@ aircraft.fuselage.diameter = 5.4;
 
 aircraft.wing.AR          = 8.5;
 aircraft.wing.taper_ratio = 0.35;
+aircraft.wing.sweep = 30;
 
 aircraft.ht.VolCoeff      = 0.40;
 aircraft.ht.AR            = 4.0;
 aircraft.ht.TaperRatio    = 0.60;
 aircraft.ht.leverArm_frac = 0.45;
+aircraft.ht.sweep = 30;
 
 aircraft.vt.VolCoeff      = 0.04;
 aircraft.vt.AR            = 1.8;
 aircraft.vt.TaperRatio    = 0.70;
 aircraft.vt.leverArm_frac = 0.40;
 aircraft.vt.twinTail      = true;
+aircraft.vt.sweep = 35;
 
 % Empty Weight Buildup
+aircraft.constants.limitLoad = 8;
+aircraft.constants.ultLoad = 1.5 * aircraft.constants.limitLoad;
+aircraft.constants.maxMach = 1.6;
+
+
 
 % CG and Inertia
+aircraft.engine.cg.x = 45;
+aircraft.cockpit.cg.x = 8;
+
 
 % Landing Gear
+aircraft.gear.mg.extendedLength = 40;   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% fixxxxxxxxxxxxxxxxxxxx
+aircraft.gear.ng.extendedLength = 40;
 
 % Time-Step Mission
 aircraft.weight.tolerance = 0.06; % GO TO getConfig to uncomment
