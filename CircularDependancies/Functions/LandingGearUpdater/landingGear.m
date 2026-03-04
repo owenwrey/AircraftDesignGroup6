aa4b2b1e3159403900e4093117ff6a03ea517837
acarpet <flyingcarpetjunior@gmail.com>
2026-03-03 21:33:57 -0600

    name changes

--- CircularDependancies/Functions/LandingGearUpdater/landingGear.m
+++ CircularDependancies/Functions/LandingGearUpdater/landingGear.m
@@ -7,15 +7,15 @@ WB = 0.8.* Aircraft.fuselage.length;
 J = WB./10; % main gear moment arm
 K = WB.*(9/10); % front gear moment arm
 
-Aircraft.mainGear.x = Aircraft.cg.x + J;
-Aircraft.frontGear.x = Aircraft.cg.x - K;
+Aircraft.gear.mg.x = Aircraft.cg.x + J;
+Aircraft.gear.ng.x = Aircraft.cg.x - K;
 
-Aircraft.mainGear.height = (Aircraft.fuselage.length - Aircraft.mainGear.x).*tand(tipBackAngle);
-Aircraft.frontGear.height = Aircraft.mainGear.height; 
+Aircraft.gear.mg.height = (Aircraft.fuselage.length - Aircraft.gear.mg.x).*tand(tipBackAngle);
+Aircraft.gear.ng.height = Aircraft.gear.mg.height; 
 
 h_wingtip = Aircraft.cg.z;  
 b_2 = Aircraft.wing.span/2; 
-Aircraft.mainGear.width = 2*(b_2 - (h_wingtip-0.5))./(tand(5)); % 5 degree roll
+Aircraft.gear.mg.width = 2*(b_2 - (h_wingtip-0.5))./(tand(5)); % 5 degree roll
 
 
 end
