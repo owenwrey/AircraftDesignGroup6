a549214df12f6b5ee53419b8d06cce9320250850
owenwrey <owenwrey@gmail.com>
2026-04-06 13:33:50 -0500

    Renaming GA folders for new run

--- Genetic Algorithm Sizing Framework/SizingFrameworkApr06.m
+++ Genetic Algorithm Sizing Framework/SizingFrameworkApr06.m
@@ -1,13 +1,14 @@
-function aircraft = SizingFrameworkMar24(input)
-% Circular Dependancies Solver Main
+function aircraft = SizingFrameworkApr06(input)
+% Circular Dependancies Solver Main equation
 % Aircraft Design - Chakraborty
 % Group 6
 %--------------------------------------------------------------------------
+clc; clearvars; close all
 
-addpath(genpath('FunctionsMar24')); % lets matlab see all the functions within Functions folder
+addpath(genpath('Functions')); % lets matlab see all the functions within Functions folder
 
-%% Mission Select
 displayResults = false;
+%% Mission Select
 missionToRun = "Strike"; % either 'A2A', or 'Strike', or 'Both' to use the constraining mission
 % missionToRun = "A2A";
 
@@ -57,8 +58,8 @@ aircraft.fuselage.cg.y = 0;
 aircraft.fuselage.cg.z = 10;
 
 aircraft.wing.AR          = input(2);
-aircraft.wing.taper_ratio = 0.25;
-aircraft.wing.sweep = 30;
+aircraft.wing.taper_ratio = input(3);
+aircraft.wing.sweep = input(4);
 aircraft.wing.T2C = .055;   % thickness to chord
 aircraft.wing.l = 50;
 aircraft.wing.x_c = .24;
@@ -86,6 +87,7 @@ aircraft.avionics.weight = 2500;
 
 % Empty Weight Buildup
 aircraft.constants.limitLoad = 8;
+aircraft.constants.negLimitLoad = -3;
 aircraft.constants.ultLoad = 1.5 * aircraft.constants.limitLoad;
 aircraft.constants.maxMach = 1.6;
 
@@ -119,9 +121,7 @@ iterationMax = 1000;
 
 while( not(exitFlag) && iteration <= iterationMax )
     iteration = iteration + 1;
-    if displayResults == true
-        fprintf("   Iteration: %u\n", iteration);
-    end
+    fprintf("   Iteration: %u\n", iteration);
     aircraftOld = aircraft;
 
     %% -| Geometry Updater |-----------------------------------------------
@@ -137,7 +137,7 @@ while( not(exitFlag) && iteration <= iterationMax )
     %----------------------------------------------------------------------
 
     %% -| CG and Inertia Calculator |--------------------------------------
-     aircraft = CgInertiaCalc(aircraft);
+    aircraft = CgInertiaCalc(aircraft);
     %----------------------------------------------------------------------
 
     %% -| Landing Gear Updater |-------------------------------------------
@@ -175,16 +175,41 @@ while( not(exitFlag) && iteration <= iterationMax )
     aircraft.fuelSys.VP = aircraft.constants.fuelVolume/2;  % self-sealing tanks volume, gal
     aircraft.constants.thrustToWeight_TO.AB = 2*aircraft.engine.thrust/aircraft.weight.total;
     aircraft.constants.thrustToWeight_TO.mil = 2*aircraft.engine.thrustMil/aircraft.weight.total;
+    aircraft.constants.EWF = aircraft.weight.empty/aircraft.weight.total;
 
 end
 
-savename = strrep(sprintf('Aircraft_WL%0.4f_AR%0.4f',aircraft.constants.wingLoading, aircraft.wing.AR),'.','_');
+%% Wing Loading---Thrust-to-Weight check
+WL_polyPoints = [   50,     62,     74,     75,    106,   106,    50];
+TW_polyPoints = [1.211, 0.9784, 0.8216, 0.8149, 0.7379, 1.211, 1.211];
+
+% figure;
+% plot(WL_polyPoints, TW_polyPoints); hold on; % check the shape of the design space
+% plot(aircraft.constants.wingLoading, aircraft.constants.thrustToWeight_TO.mil, 'Marker','o')
+
+[inWL_TWdesignSpace, onWL_TWdesignSpace] = inpolygon(aircraft.constants.wingLoading, aircraft.constants.thrustToWeight_TO.mil,...
+                                                WL_polyPoints, TW_polyPoints); % this checks if the W/S-T/W combo is valid
+
+if ~inWL_TWdesignSpace || ~onWL_TWdesignSpace % if not inside or on the border of the W/S T/W design space...
+    % saves converged weight (+other obj funcs) in case we want to know what the bad design looked like anyway
+    aircraft.constants.warnings.totalWeight = aircraft.weight.total;
+    aircraft.constants.warnings.EWF = aircraft.constants.EWF;
+
+    % makes weight 1 trillion pounds (bad)
+    aircraft.weight.total = 10e12; % makes weight 1 trillion pounds (bad)
+    aircraft.constants.EWF = 1; % makes aircraft all empty weight (bad)
+
+    % SET ANY OTHER OBJECTIVE FUNCTION HERE, SET TO SOMETHING REALLY BAD
+
+    % save a wanring in the ac struct
+    aircraft.constants.warnings.WL_TW = 'Wing Loading-Thrust to Weight combo does not meet point performance requirements'
+end
+
+savename = strrep(sprintf('Veh%0.4f_AR%0.4f',aircraft.constants.wingLoading, aircraft.wing.AR),'.','_');
 
 aircraft.aero.cd0 = "A function was here but was replaced to make gamultiobj happy";
 
 save(savename,'aircraft')
-
-
 
 % ignore this, just helps with report writing
 % cell2mat(struct2cell(aircraft.engine.structures))
@@ -193,21 +218,25 @@ save(savename,'aircraft')
 % miscPerc = cell2mat(struct2cell(aircraft.weight.misc))./aircraft.weight.total
 
 %% -| Display Results |----------------------------------------------------
-if displayResults == true
-fprintf("\n Converged after %u iterations\n\n", iteration)
-fprintf("  W0/S:      %.0f psf\n", aircraft.constants.wingLoading)
-fprintf("  (T/W0)ab:  %.2f\n", aircraft.constants.thrustToWeight_TO.AB)
-fprintf("  (T/W0)mil: %.2f\n", aircraft.constants.thrustToWeight_TO.mil)
-fprintf("---------------------\n")
-fprintf("  MTOW: %.0f lb\n", aircraft.weight.total)
-fprintf("  EOW:  %.0f lb\n", aircraft.weight.empty)
-fprintf("  CG_x: %.3f ft\n", aircraft.cg.x)
-fprintf("  CG_y: %.3f ft\n", aircraft.cg.y)
-fprintf("  CG_z: %.3f ft\n", aircraft.cg.z)
-fprintf('---------------------\n\n')
+if displayResults
+    fprintf("\n Converged after %u iterations\n\n", iteration)
+    fprintf("  W0/S:      %.0f psf\n", aircraft.constants.wingLoading)
+    fprintf("  (T/W0)ab:  %.2f\n", aircraft.constants.thrustToWeight_TO.AB)
+    fprintf("  (T/W0)mil: %.2f\n", aircraft.constants.thrustToWeight_TO.mil)
+    fprintf("---------------------\n")
+    fprintf("  MTOW: %.0f lb\n", aircraft.weight.total)
+    fprintf("  EOW:  %.0f lb\n", aircraft.weight.empty)
+    fprintf("  CG_x: %.3f ft\n", aircraft.cg.x)
+    fprintf("  CG_y: %.3f ft\n", aircraft.cg.y)
+    fprintf("  CG_z: %.3f ft\n", aircraft.cg.z)
+    fprintf('---------------------\n\n')
 end
 %--------------------------------------------------------------------------
 
-% clear f k timerFields data tics iterationMax;
+clear f k timerFields data tics iterationMax;
+
+% plot cg envelope
+% VnDiagram       % generate Vn diagram for converged aircraft
+% New             % generate Min TTC graph for converged aircraft  
 
 end
