b069ef0cc5d780c5eb5eb38f2ab7d2e4cdc96979
Kyle <kylekas@comcast.net>
2026-04-09 17:13:36 -0500

    yuh2 E.B.

--- Genetic Algorithm Sizing Framework/cleanup.m
+++ Genetic Algorithm Sizing Framework/cleanup.m
@@ -3,20 +3,13 @@
 %--------------------------------------------------------------------------
 clc; clear; close all
 
-files1 = dir('Aircraft_WL*');
+files1 = dir('Generation*.mat');
 if ~isempty(files1)
 delete(files1.name)
 end
-files2 = dir('Generation*.mat');
+files2 = dir('Veh*.mat');
 if ~isempty(files2)
 delete(files2.name)
 end
-folders1 = dir("Generation*");
 
-folders1 = folders1([folders1.isdir]);
-
-if ~isempty(folders1)
-for i = [string(folders1.name)]
-    rmdir(i)
-end
 end
